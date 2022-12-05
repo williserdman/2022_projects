da5e5e97777c058b47624dc9f5a2eb2fc2426c02
williserdman <willis.erdman@gmail.com>
2022-12-04 20:33:27 -0800

    Messing around w/ creating my own pseudo OS.

--- 3dPortfolio/src/routes/screen/+page.svelte
+++ 3dPortfolio/src/routes/screen/+page.svelte
@@ -1,7 +1,33 @@
-<p contenteditable="true">hello world</p>
+<script>
+	// @ts-ignore
+	import OSNavbar from "$lib/OSNavbar.svelte";
+	// @ts-ignore
+	import MainOS from "$lib/MainOS.svelte";
+</script>
+
+<html lang="html">
+	<body>
+		<div class="os-navbar">
+			<OSNavbar />
+		</div>
+		<div class="main-os">
+			<MainOS />
+		</div>
+	</body>
+</html>
 
 <style>
 	* {
 		outline: none;
+		overflow: hidden;
+	}
+	html {
+		background-color: aquamarine;
+		padding: 0;
+		margin: 0;
+		border: 0;
+	}
+	body {
+		margin: 0;
 	}
 </style>
