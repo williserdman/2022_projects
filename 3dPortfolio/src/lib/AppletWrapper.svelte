745ca2bb6069f5ed0d5b2c3a5c2908d5418601a2
williserdman <willis.erdman@gmail.com>
2022-12-17 15:00:00 -0800

    Got dragging windows working at the cost of the selector now being broken.

--- 3dPortfolio/src/lib/AppletWrapper.svelte
+++ 3dPortfolio/src/lib/AppletWrapper.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	export let type: String; // prop caught by svelte... ignore squiglies, vscode doesn't know what's up
-	export let width: number = 600;
-	export let height: number = 400;
+	export let width: number = 60;
+	export let height: number = 40;
 	export let minimized: boolean = false;
-	console.log(type);
+	export let left = 100;
+	export let top = 100;
+	console.log(width, height, minimized);
+	let moving = false;
+
+	function mouseDownTop() {
+		console.log("click 1");
+		moving = true;
+	}
+	function mouseUpTop() {
+		moving = false;
+	}
+	function mouseMoveTop(e: object) {
+		if (moving) {
+			//@ts-ignore
+			left += e.movementX;
+			//@ts-ignore
+			top += e.movementY;
+		}
+	}
 </script>
 
 <!-- top nav area of applet 
@@ -11,11 +30,27 @@
 if things from svelte
 
 bottom stuff from applet (if needed idk yet) -->
-
-<main bind:clientWidth={width} bind:clientHeight={height} />
+<html lang="html" style="left: {left}px; top: {top}px;">
+	<div
+		class="top"
+		on:mousedown={mouseDownTop}
+		on:mouseup={mouseUpTop}
+		on:mousemove={mouseMoveTop}
+		on:mouseleave={mouseUpTop}
+	/>
+	<main bind:clientWidth={width} bind:clientHeight={height} hidden={minimized} />
+</html>
 
 <style>
-	main {
+	.top {
+		height: 50px;
+		width: 100%;
 		background-color: white;
+	}
+	html {
+		position: relative;
+		width: 400px;
+		height: 400px;
+		background-color: rgba(0, 0, 100, 0.5);
 	}
 </style>
