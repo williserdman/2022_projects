efeeb2f227e1d4ca432ce2402ea5c6167b42c697
williserdman <willis.erdman@gmail.com>
2022-12-31 20:19:08 -0800

    Added tooltip

--- 3dPortfolio/src/routes/+page.svelte
+++ 3dPortfolio/src/routes/+page.svelte
@@ -1,16 +1,26 @@
 <script lang="ts" type="module">
 	import { onMount } from "svelte";
 	import { init } from "./main";
+	import "bulma";
 
 	onMount(() => {
 		init();
 	});
 </script>
 
-<html lang="html">
-	<div class="trigger" />
-	<div id="css" />
-	<div id="webgl" />
+<html lang="html" style="height: 800%;">
+	<div class="columns" />
+	<p
+		class="column box is-one-fifth m-3 has-text-centered"
+		style="color: black; z-index: 99; position: absolute;"
+	>
+		Scroll to begin...
+	</p>
+	<div style="height: 800%;">
+		<div class="trigger" />
+		<div id="css" />
+		<div id="webgl" />
+	</div>
 </html>
 
 <style>
@@ -34,5 +44,6 @@
 		position: fixed;
 		top: 0;
 		left: 0;
+		z-index: -1;
 	}
 </style>
