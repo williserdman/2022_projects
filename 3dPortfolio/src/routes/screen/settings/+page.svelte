7ca96a3b4eb020d98e0d2d8108903491046b2f73
williserdman <willis.erdman@gmail.com>
2023-04-05 13:15:54 -0700

    working on settings page

---
+++ 3dPortfolio/src/routes/screen/settings/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import "bulma";
+</script>
+
+<html lang="en">
+	<div class="columns">
+		<section class="column is-one-half is-justify-content-center is-align-content-center m-4">
+			<div class="box">p</div>
+		</section>
+		<section class="column" style="background-color: blue;">p</section>
+	</div>
+</html>
